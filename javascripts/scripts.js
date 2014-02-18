61af9041193dc56f6e3187de9856c6dfe9508496
princesspretzel <brittanga@gmail.com>
2014-02-17 21:28:11 -0500

    click functionality, some styling

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -5,24 +5,29 @@ window.onload=function() {
     $('.replacement').show();
   }, 1000);
 
+  $('#about').on("click", function(){
+    $('.poofle').hide();  
+    $('.about').show();
+  });
+
+  $('#portfolio').on("click", function(){
+    $('.poofle').hide();
+    $('.portfolio').show();
+  });
+
+  $('#home').on("click", function(){
+    $('.poofle').hide();
+    $('.home').show();
+  });
+
+  $('#skills').on("click", function(){
+    $('.poofle').hide();
+    $('.skills').show();
+  });
+
+  $('#contact').on("click", function(){
+    $('.poofle').hide();
+    $('.contact').show();
+  });
+
 };
-
-$('#about').on("click", function(){
-  $('.about').show();
-});
-
-$('#portfolio').on("click", function(){
-  $('.portfolio').show();
-});
-
-$('#home').on("click", function(){
-  $('.home').show();
-});
-
-$('#skills').on("click", function(){
-  $('.skills').show();
-});
-
-$('#contact').on("click", function(){
-  $('.contact').show();
-});
