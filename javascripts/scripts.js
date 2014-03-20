beb337d4636437df91c6c9a46631625747bf2de6
princesspretzel <brittanga@gmail.com>
2014-03-20 19:52:56 -0400

    added owl carousel files, changed type

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -24,16 +24,18 @@ window.onload=function() {
     $('.poofle').hide();
     $('.portfolio').show();
   });
-
-  // $(".projects").on("click", function(){ 
-  //   $('.projects').hide();
-  //   $('.group').show();
-  // }); 
-
-  // $(".projects").on("click", function(){ 
-  //   $('.projects').hide();
-  //   $('.game1').show();
-  // }); 
+ 
+  $("#projects").owlCarousel({
+    items: 2,
+    itemsCustom : false,
+    itemsDesktop : [1199,4],
+    itemsDesktopSmall : [980,3],
+    itemsTablet: [768,2],
+    itemsTabletSmall: false,
+    itemsMobile : [479,1],
+    singleItem : false,
+    itemsScaleUp : false
+  });
 
   $('#home').on("click", function(){
     $('.poofle').hide();
